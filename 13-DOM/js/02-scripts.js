db68e99fcac90a37685f4b796c0a4fd611367418
XavZol <alansolis707@gmail.com>
2026-05-29 03:10:54 -0600

    Reforzando Javascript

---
+++ 13-DOM/js/02-scripts.js
@@ -0,0 +1,15 @@
+// Seleccionar elementos por su clase...
+
+const header = document.getElementsByClassName('header');
+console.log(header);
+
+const hero = document.getElementsByClassName('hero')
+console.log(hero);
+
+// Si las calses existen más de una vex los cont
+const contenedores = document.getElementsByClassName('contenedor')
+console.log(contenedores); 
+
+const noExiste = document.getElementsByClassName('no-existe');
+console.log(noExiste);
+
